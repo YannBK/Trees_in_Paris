9d8881fd2999feef8906cb66355ece58136becdf
YannBK <61941063+YannBK@users.noreply.github.com>
2022-01-16 06:12:19 +0100

    tableau cliquable

--- api.js
+++ api.js
@@ -5,19 +5,39 @@ let datas;
 let source = 0; //pour rafraichir la map
 let map;
 let arrArbresTableau = [];
+let arrArbres = [];
+let myPopUp;
 
 //class pour création des arbres => finiront dans arrArbres, qui finira dans les datas à afficher
 class Arbres {
-    constructor(adresse, circonf, date, esp, genre, haut, francais, coord, variete) {
+    constructor(adresse, circonf, date, esp, genre, haut, francais, coord, variete, id) {
         this.type = 'Feature',
             this.properties = {
                 description:
-                    `<h3>${genre} ${esp} ${variete}</h3><p>${francais}</p><p>Adresse : ${adresse}</p><p>Hauteur : ${haut}m, Circonférence : ${circonf}cm</p><p>Année de plantation : ${date}</p>`
+                    `<h3>${genre} ${esp} ${variete}</h3><p>${francais}</p><p>Adresse : ${adresse}</p><p>Hauteur : ${haut}m, Circonférence : ${circonf}cm</p><p>Année de plantation : ${date}</p>`,
+
             },
             this.geometry = {
                 type: 'Point',
                 coordinates: coord
             }
+        this.id = id
+    }
+}
+
+//décolorisation des lignes
+function coloriseRow(ligne){
+    let oldRow = ligne || document.querySelector(`[style="background-color: rgb(255, 150, 150);"]`);
+    if (oldRow) {
+        if (((oldRow.nextSibling) && (oldRow.nextSibling.style.backgroundColor == "rgb(17, 17, 17)"))
+            ||
+            ((oldRow.previousSibling) && (oldRow.previousSibling.style.backgroundColor == "rgb(17, 17, 17)"))
+        ) {
+            oldRow.style.backgroundColor = "transparent";
+        }
+        else {
+            oldRow.style.backgroundColor = "rgb(17, 17, 17)";
+        }
     }
 }
 
@@ -40,6 +60,11 @@ function miseAJour(src) {
         }
     });
     map.on('click', `${src}`, (e) => {
+        //couleur sur la ligne de l'arbre sélectionné
+        coloriseRow()
+        let row = document.querySelector(`[data-number='${e.features[0].id}']`);
+        row.style.backgroundColor = "rgb(255,150,150)";
+
         // Copy coordinates array.
         const coordinates = e.features[0].geometry.coordinates.slice();
         const description = e.features[0].properties.description;
@@ -49,15 +74,17 @@ function miseAJour(src) {
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
-        new mapboxgl.Popup()
+        myPopUp = new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(description)
             .addTo(map);
     });
+    
     //curseur=pointeur dans la map
     map.on('mouseenter', `${src}`, () => {
         map.getCanvas().style.cursor = 'pointer';
     });
+
     //et l'inverse
     map.on('mouseleave', `${src}`, () => {
         map.getCanvas().style.cursor = '';
@@ -66,13 +93,15 @@ function miseAJour(src) {
 
 //création des datas de la map
 async function createData(tri) {
+
     //récupération données
     let url = `https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis&q=&rows=10000&facet=libellefrancais&facet=genre&facet=espece&facet=stadedeveloppement&facet=varieteoucultivar&facet=dateplantation`;
     const data = await fetch(url);
     const json = await data.json();
-    console.log(json);
+    // console.log(json);
+
     //création des objets
-    let arrArbres = [];
+    arrArbres = [];
     arrArbresTableau = [];
     let selectMin = parseInt(document.getElementById('selectmin').value);
     let selectMax = parseInt(document.getElementById('selectmax').value);
@@ -91,10 +120,12 @@ async function createData(tri) {
                 if (json.records[i].fields.varieteoucultivar != undefined) {
                     variet = json.records[i].fields.varieteoucultivar;
                 }
-                arbre = new Arbres(json.records[i].fields.adresse, json.records[i].fields.circonferenceencm, annee, json.records[i].fields.espece, json.records[i].fields.genre, json.records[i].fields.hauteurenm, json.records[i].fields.libellefrancais, json.records[i].geometry.coordinates, variet);
+                arbre = new Arbres(json.records[i].fields.adresse, json.records[i].fields.circonferenceencm, annee, json.records[i].fields.espece, json.records[i].fields.genre, json.records[i].fields.hauteurenm, json.records[i].fields.libellefrancais, json.records[i].geometry.coordinates, variet, i);
+
                 //pour l'affichage du tableau
-                arrArbresTableau.push(json.records[i].fields);
+                arrArbresTableau.push([i, json.records[i].fields]);
             }
+
             //lorsqu'un tri est demandé
             else {
                 if (Object.values(json.records[i].fields).includes(tri)) {
@@ -102,10 +133,10 @@ async function createData(tri) {
                     if (json.records[i].fields.varieteoucultivar != undefined) {
                         variet = json.records[i].fields.varieteoucultivar;
                     }
-                    arbre = new Arbres(json.records[i].fields.adresse, json.records[i].fields.circonferenceencm, annee, json.records[i].fields.espece, json.records[i].fields.genre, json.records[i].fields.hauteurenm, json.records[i].fields.libellefrancais, json.records[i].geometry.coordinates, variet);
-                    //pour l'affichage du tableau
+                    arbre = new Arbres(json.records[i].fields.adresse, json.records[i].fields.circonferenceencm, annee, json.records[i].fields.espece, json.records[i].fields.genre, json.records[i].fields.hauteurenm, json.records[i].fields.libellefrancais, json.records[i].geometry.coordinates, variet, i);
 
-                    arrArbresTableau.push(json.records[i].fields);
+                    //pour l'affichage du tableau
+                    arrArbresTableau.push([i, json.records[i].fields]);
                 }
             }
         }
@@ -122,9 +153,9 @@ async function createData(tri) {
     //pour gérer les sources identiques de la map
     source++;
     //pour gérer le tri des variétés (ou presque)
-    for(let i = 0; i<arrArbresTableau.length; i++){
-        if(arrArbresTableau[i].varieteoucultivar==undefined){
-            arrArbresTableau[i].varieteoucultivar = " ";
+    for (let i = 0; i < arrArbresTableau.length; i++) {
+        if (arrArbresTableau[i][1].varieteoucultivar == undefined) {
+            arrArbresTableau[i][1].varieteoucultivar = " ";
         }
     }
 
@@ -153,6 +184,7 @@ let modulo = {
 
 //création tableau des données affichées
 function tableau(arr) {
+
     //réinitialisation de la zone
     let divTableau = document.getElementById('tableau');
     let titre = document.querySelector('h2');
@@ -176,50 +208,75 @@ function tableau(arr) {
     let i = 0;
     arr.map(el => {
         let annee = "";
-        let arrond = titleCase(el.arrondissement);
+        let arrond = titleCase(el[1].arrondissement);
         let variet = "";
-        let adress = titleCase(el.adresse);
-        let domain = el.domanialite;
+        let adress = titleCase(el[1].adresse);
+        let domain = el[1].domanialite;
         //reformatage de la date
         for (let k = 0; k < 4; k++) {
-            annee += el.dateplantation[k];
+            annee += el[1].dateplantation[k];
         }
         //reformatage arrondissement
-        if (el.arrondissement.includes(' ARRDT')) {
-            arrond = titleCase(el.arrondissement.replace(' ARRDT', ""));
+        if (el[1].arrondissement.includes(' ARRDT')) {
+            arrond = titleCase(el[1].arrondissement.replace(' ARRDT', ""));
         }
         //reformatage variété
-        if (el.varieteoucultivar != undefined) {
-            variet = el.varieteoucultivar;
+        if (el[1].varieteoucultivar != undefined) {
+            variet = el[1].varieteoucultivar;
         }
         //reformatage adresse
-        if (el.adresse.includes(' D ')) {
-            adress = titleCase(el.adresse.replace(' D ', " D'"));
+        if (el[1].adresse.includes(' D ')) {
+            adress = titleCase(el[1].adresse.replace(' D ', " D'"));
         }
-        else if (el.adresse.includes('L ')) {
-            adress = titleCase(el.adresse.replace(' L ', " L'"));
+        else if (el[1].adresse.includes('L ')) {
+            adress = titleCase(el[1].adresse.replace(' L ', " L'"));
         }
         //reformatage domanialité
-        if (el.domanialite.includes('CIMETIERE')) {
-            domain = titleCase(el.domanialite);
+        if (el[1].domanialite.includes('CIMETIERE')) {
+            domain = titleCase(el[1].domanialite);
         }
         //enfin la ligne! d'où l'intérêt de créer des bases de données avec des données uniformisées et exploitables
         let tr = document.createElement('tr');
+        tr.setAttribute('data-number', el[0]);
         if (i % 2 == 0) {
             tr.style.backgroundColor = "#111";
         }
+        i++;//d'où l'intérêt de map vs for, mais j'ai du mal m'y prendre
 
         let ligne = "";
         if (document.body.clientWidth < 1000) {
-            ligne = `<td>${adress}</td><td>${arrond}</td><td>${el.libellefrancais}</td><td>${el.genre}</td><td>${el.espece}</td><td>${toString(el.hauteurenm)}</td><td>${el.circonferenceencm}</td><td>${annee}</td>`;
+            ligne = `<td>${adress}</td><td>${arrond}</td><td>${el[1].libellefrancais}</td><td>${el[1].genre}</td><td>${el[1].espece}</td><td>${toString(el[1].hauteurenm)}</td><td>${el[1].circonferenceencm}</td><td>${annee}</td>`;
         }
         else {
-            ligne = `<td>${adress}</td><td>${arrond}</td><td>${el.libellefrancais}</td><td>${el.genre}</td><td>${el.espece}</td><td>${variet}</td><td>${el.hauteurenm}</td><td>${el.circonferenceencm}</td><td>${annee}</td><td>${domain}</td>`;
+            ligne = `<td>${adress}</td><td>${arrond}</td><td>${el[1].libellefrancais}</td><td>${el[1].genre}</td><td>${el[1].espece}</td><td>${variet}</td><td>${el[1].hauteurenm}</td><td>${el[1].circonferenceencm}</td><td>${annee}</td><td>${domain}</td>`;
         }
         tr.innerHTML = ligne;
 
         table.appendChild(tr);
-        i++;//d'où l'intérêt de map vs for, mais j'ai du mal m'y prendre
+        
+        
+        tr.addEventListener('click',function(){
+            //affichage popup associée
+            if(myPopUp!=undefined){
+                myPopUp.remove();
+            }
+            let coord;
+            let desc;
+            for(let i = 0; i<arrArbres.length;i++){
+                if(arrArbres[i].id==this.dataset.number){
+                    desc = arrArbres[i].properties.description;
+                    coord = arrArbres[i].geometry.coordinates;
+                }
+            }
+            myPopUp = new mapboxgl.Popup()
+            .setLngLat(coord)
+            .setHTML(desc)
+            .addTo(map);
+
+            //colorisation de la ligne
+            coloriseRow()
+            this.style.backgroundColor = "rgb(255,150,150)";
+        })
     });
 
     divTableau.appendChild(table);
@@ -230,20 +287,20 @@ function tableau(arr) {
         el.addEventListener('click', function () {
             let arrT;
             if (modulo[this.id] % 2 == 0) {
-                if(this.id!='hauteurenm' && this.id!='circonferenceencm'){
-                    arrT = arr.sort((a, b) => a[this.id].localeCompare(b[this.id]))
+                if (this.id != 'hauteurenm' && this.id != 'circonferenceencm') {
+                    arrT = arr.sort((a, b) => a[1][this.id].localeCompare(b[1][this.id]))
                 }
-                else{
-                    arrT = arr.sort((a, b) => a[this.id] - b[this.id]);
+                else {
+                    arrT = arr.sort((a, b) => a[1][this.id] - b[1][this.id]);
                 }
                 modulo[this.id]++;
             }
-            else{
-                if(this.id!='hauteurenm' && this.id!='circonferenceencm'){
-                    arrT = arr.sort((a, b) => b[this.id].localeCompare(a[this.id]))
+            else {
+                if (this.id != 'hauteurenm' && this.id != 'circonferenceencm') {
+                    arrT = arr.sort((a, b) => b[1][this.id].localeCompare(a[1][this.id]))
                 }
-                else{
-                    arrT = arr.sort((a, b) => b[this.id] - a[this.id]);
+                else {
+                    arrT = arr.sort((a, b) => b[1][this.id] - a[1][this.id]);
                 }
                 modulo[this.id]++;
             }
@@ -254,6 +311,7 @@ function tableau(arr) {
 
 //Création select par nom
 function creerSelects(obj, id, cat, lab) {
+
     //création html des box-div
     let box = document.createElement('div');
     box.setAttribute('id', `${id}${cat}`);
@@ -326,6 +384,7 @@ function creerSelects(obj, id, cat, lab) {
 
 //création selects de "range" pour circonférence
 function creerSelectsCirconf(ids, lab) {
+
     //création html
     for (let i = 0; i < ids.length; i++) {
         let labelGenre = document.createElement('label');
@@ -333,6 +392,7 @@ function creerSelectsCirconf(ids, lab) {
         labelGenre.textContent = `${lab[i]} (cm)`;
         let htmlSelectGenre = document.createElement('select');
         htmlSelectGenre.setAttribute('id', ids[i]);
+
         //création options
         for (let k = 0; k < 701; k += 50) {
             if ((ids[i] == 'selectmin' && k != 700) || (ids[i] == 'selectmax' && k != 0)) {
@@ -368,6 +428,7 @@ function creerSelectsCirconf(ids, lab) {
             else {
                 value = 'tout';
             }
+
             //empêcher le min d'être supérieur au max, ou le max d'être inférieur au min, selon votre vision du monde
             let selectMin = document.getElementById('selectmin');
             let selectMax = document.getElementById('selectmax');
@@ -380,6 +441,7 @@ function creerSelectsCirconf(ids, lab) {
                 }
             }
 
+            //création données + affichage
             await createData(value);
             miseAJour(source);
             tableau(arrArbresTableau);
@@ -389,12 +451,13 @@ function creerSelectsCirconf(ids, lab) {
 
 //au click du bouton de la page d'intro, affichage et création de la totalité
 btnIntro.addEventListener('click', async function () {
-    const resultat = document.getElementById('resultat');
+    
     //récupération données
     let url = `https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis&q=&rows=10000&facet=libellefrancais&facet=genre&facet=espece&facet=stadedeveloppement&facet=varieteoucultivar&facet=dateplantation`;
     const data = await fetch(url);
     const json = await data.json();
-    console.log(json)
+    console.log(json);
+
     //création nav + map + pied
     let nav = document.createElement('div');
     nav.setAttribute('id', 'nav');
@@ -402,15 +465,19 @@ btnIntro.addEventListener('click', async function () {
     divMap.setAttribute('id', 'map');
     let pied = document.createElement('div');
     pied.setAttribute('id', 'pied');
-    //insertion nav + map
+
+    //insertion nav + map + pied
+    const resultat = document.getElementById('resultat');
     resultat.appendChild(nav);
     resultat.appendChild(divMap);
     resultat.appendChild(pied);
+
     //zoom de la map selon écran
     let zoomy = 11;
     if (document.body.clientWidth < 700) {
         zoomy = 10;
     }
+
     //initialisation map
     mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmJldHQiLCJhIjoiY2t5Y3B4OXJvMGFocTJ2cm04eGUzMGlobCJ9.3Ukk2dIMB7THGu_fNhX4-A';
     map = new mapboxgl.Map({
@@ -419,8 +486,10 @@ btnIntro.addEventListener('click', async function () {
         center: [2.34, 48.86],
         zoom: zoomy
     });
-    //ajout zoom et +
+
+    //ajout contrôles
     map.addControl(new mapboxgl.NavigationControl());
+
     //changement icone
     map.loadImage(
         '22330deciduoustree_98753.png',
@@ -429,11 +498,13 @@ btnIntro.addEventListener('click', async function () {
             map.addImage('tree', image);
         }
     )
+    
     //création select
     creerSelects(json, 'selectgenre', 'genre', 'Genre')
     creerSelects(json, 'selectfrance', 'libellefrancais', 'Nom français');
     creerSelectsCirconf(['selectmin', 'selectmax'], ['Circonférence min', 'Circonférence max']);
-
+    
+    //création données
     await createData('tout');
 
     //animations fun, ou pas
@@ -443,6 +514,7 @@ btnIntro.addEventListener('click', async function () {
         btnIntro.style.display = "none";
     }, 500)
 
+    //affichage
     miseAJour(source);
     tableau(arrArbresTableau);
 })
